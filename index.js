62c0f318e8d359dab4f14e8becbec5ef991aa44d
caseytansey <ctansey12600@outlook.com>
2021-08-18 11:33:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    const array = name
+    
+    for(let i = 0; i < array.length; i++){
+        return array[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`
+    }
+
+    return array
+}
